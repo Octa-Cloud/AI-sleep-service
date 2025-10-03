4c3d98c1d2ae787b526d7f47a92559961fb1dd55
tmin002 <ssh9930@gmail.com>
2025-10-03 14:48:29 +0900

    refactor: Exception 반환코드 추가

--- app/api/common/exception/custom/auth_exceptions.py
+++ app/api/common/exception/custom/auth_exceptions.py
@@ -6,10 +6,12 @@ from app.api.common.exception.api_exception import ApiException
 class UnauthorizedApiException(ApiException):
     status_code: int = 401
     message: str = "인증이 필요합니다."
+    code_key: str = "AUTH"
 
 
 class UserNotFoundApiException(ApiException):
     status_code: int = 404
     message: str = "사용자를 찾을 수 없습니다."
+    code_key: str = "AUTH"
 
 
--- app/api/common/exception/custom/brainwave_exceptions.py
+++ app/api/common/exception/custom/brainwave_exceptions.py
@@ -7,20 +7,24 @@ from app.api.common.exception.custom.analyze_fail_exception import AnalyzeFailEx
 class BrainwaveAnalyzeFailApiException(AnalyzeFailException):
     status_code: int = 500
     message: str = "뇌파 분석에 실패했습니다."
+    code_key: str = "BRAIN"
 
 
 class BrainwaveFormatValidationFailApiException(ApiException):
     status_code: int = 400
     message: str = "올바르지 않은 파일 형태입니다."
+    code_key: str = "BRAIN_FORMAT"
 
 
 class BrainwaveChannelValidationFailApiException(ApiException):
     status_code: int = 400
     message: str = "올바르지 않은 채널입니다."
+    code_key: str = "BRAIN_CHANNEL"
 
 
 class BrainwaveLengthValidationFailApiException(ApiException):
     status_code: int = 400
     message: str = "데이터의 길이가 청크 사이즈를 초과합니다."
+    code_key: str = "BRAIN_LENGTH"
 
 
