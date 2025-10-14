b22cfb62366d0064835f41977767c9c3cf66c86d
tmin002 <ssh9930@gmail.com>
2025-10-14 19:39:18 +0900

    feat: 실행 파일에 소리 분석 도메인 코드 추가

--- app/api/main.py
+++ app/api/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from app.common.logging import configure_logging
 from app.api.domain.presentation.router.session_router import Router as SessionRouter
 from app.api.domain.presentation.router.brainwave_router import Router as BrainwaveRouter
+from app.api.domain.presentation.router.sound_router import Router as SoundRouter
 from app.api.common.exception.exception_handler import register_exception_handlers
 from app.api.common.middleware.auth_middleware import AuthMiddleware
 from app.api.common.dependencies import container as di_container
@@ -15,6 +16,8 @@ from app.common import config
 
 from app.api.domain.worker.brainwave_chunk_splitter.__main__ import run as run_splitter  # type: ignore
 from app.api.domain.worker.brainwave_analyzer.__main__ import run as run_analyzer  # type: ignore
+from app.api.domain.worker.sound_chunk_splitter.__main__ import run as run_sound_splitter  # type: ignore
+from app.api.domain.worker.sound_analyzer.__main__ import run as run_sound_analyzer  # type: ignore
 
 
 configure_logging("fastapi-api")
@@ -34,6 +37,7 @@ async def health_check():
 
 app.include_router(SessionRouter)
 app.include_router(BrainwaveRouter)
+app.include_router(SoundRouter)
 
 _consumers = KafkaConsumerOrchestrator(container=di_container)
 
@@ -50,6 +54,8 @@ async def _start_consumers() -> None:
         app.state.worker_tasks = [
             asyncio.create_task(run_splitter()),
             asyncio.create_task(run_analyzer()),
+            asyncio.create_task(run_sound_splitter()),
+            asyncio.create_task(run_sound_analyzer()),
         ]
         await _consumers.wait_ready()
 
